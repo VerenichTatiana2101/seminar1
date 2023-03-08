d0db5e13a781e95eb5f4d1f23462b0849c65e33a
Verenich Tatiana <verenik91@gmail.com>
2023-03-09 01:26:44 +0300

    Добавлен задача 1

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая 
+// 1. на вход принимает два числа и проверяет, 
+// 2. является ли первое число квадратом второго.
+
+// Например:
+// a = 25, b = 5 -> да,
+// a = 2, b = 10 -> нет,
+// a = 9, b = -3 -> да,
+// a = -3, b = 9 -> нет.
+
+Console.Write("Введите целое число 1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число 2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == numberB * numberB)
+    {
+     Console.Write($"Квадрат числа {numberB} = {numberA}");
+    }
+
+else
+    {
+     Console.Write($"Квадрат числа {numberB} не равняется {numberA}");
+    }

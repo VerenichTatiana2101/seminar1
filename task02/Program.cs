2d43eba19533b2bfbc5a895a99ed871e678917a6
Verenich Tatiana <verenik91@gmail.com>
2023-03-09 18:41:30 +0300

    решение задачи 2

--- task02/Program.cs
+++ task02/Program.cs
@@ -4,6 +4,53 @@
 // Например 3 -> среда
 //          5 -> пятница
 
+// Console.WriteLine("Введите целое число: ");
+// string number = Console.ReadLine();
+
+// if (number == "1")
+// {
+//     Console.Write($"День недели {number} - понедельник");
+// }
+
+// else if (number == "2")
+// {
+//     Console.Write($"День недели {number} - вторник");
+// }
+
+// else if (number == "3")
+// {
+//     Console.Write($"День недели {number} - среда");
+// }
+
+// else if (number == "4")
+// {
+//     Console.Write($"День недели {number} - четверг");
+// }
+
+// else if (number == "5")
+// {
+//     Console.Write($"День недели {number} - пятница");
+// }
+
+// else if (number == "6")
+// {
+//     Console.Write($"День недели {number} - суббота");
+// }
+
+// else if (number == "7")
+// {
+//     Console.Write($"День недели {number} - воскресенье");
+// }
+
+// else
+// {
+//     Console.Write($"Дня недени {number} - не существует, введите другое число");
+// }
+
+//В данном случает else if оптимизирует наше решение, 
+//и если вариант подходит, 
+//цикл останавливается и не идет дальше по кругу.
+
 Console.Write("Введите целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
@@ -12,32 +59,32 @@ if (number == 1)
     Console.Write($"День недели {number} - понедельник");
 }
 
-if (number == 2)
+else if (number == 2)
 {
     Console.Write($"День недели {number} - вторник");
 }
 
-if (number == 3)
+else if (number == 3)
 {
     Console.Write($"День недели {number} - среда");
 }
 
-if (number == 4)
+else if (number == 4)
 {
     Console.Write($"День недели {number} - четверг");
 }
 
-if (number == 5)
+else if (number == 5)
 {
     Console.Write($"День недели {number} - пятница");
 }
 
-if (number == 6)
+else if (number == 6)
 {
     Console.Write($"День недели {number} - суббота");
 }
 
-if (number == 7)
+else if (number == 7)
 {
     Console.Write($"День недели {number} - воскресенье");
 }

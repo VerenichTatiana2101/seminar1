92aa0dae554b377adc1a57474ddab9e800245448
Verenich Tatiana <verenik91@gmail.com>
2023-03-11 02:08:08 +0300

    закоментила часть задачи

--- task05/Program.cs
+++ task05/Program.cs
@@ -8,17 +8,17 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine($"случайное число из отрезка 10-99 -> {number}");
 
-int firstDigit = number / 10;
-int secondDigit = number % 10;
-if (firstDigit > secondDigit)
-    Console.WriteLine($"Наибольшая цифра сисла -> {firstDigit}");
-else
-    Console.WriteLine($"Наибольшая цифра сисла -> {secondDigit}");
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// if (firstDigit > secondDigit)
+//     Console.WriteLine($"Наибольшая цифра сисла -> {firstDigit}");
+// else
+//     Console.WriteLine($"Наибольшая цифра сисла -> {secondDigit}");
 
-///////////////////////////////
-int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
-Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
-///////////////////////////////
+// ///////////////////////////////
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+// ///////////////////////////////
 int result = MaxDigit(number);
 Console.WriteLine($"Наибольшая цифра числа -> {result}");
 

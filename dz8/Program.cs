4ea428ea638275380a10cd99416ffca7b6ac944c
Verenich Tatiana <verenik91@gmail.com>
2023-03-10 16:06:34 +0300

    Решение задачи 8

--- dz8/Program.cs
+++ dz8/Program.cs
@@ -6,3 +6,42 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+// Console.Write("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int count = 2;
+// while (count <= N)
+//     if (count % 2 == 0)
+//     {
+//         Console.WriteLine($"{count} ");
+//     }
+//     else count++;
+// Console.WriteLine($"{count} ");
+
+// Console.Write("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// int i = 0;
+
+// for (i = 1; i <= N; i++)
+// {
+//     if (i % 2 == 0)
+//     {
+//         Console.Write(i);
+//         i++;
+//     }
+// }
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i <= N)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i} ");
+        i++;
+    }
+i++;   
+}

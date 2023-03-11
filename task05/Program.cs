ed93570a0653cd513b96924a2b9c18ecff72f5f3
Verenich Tatiana <verenik91@gmail.com>
2023-03-11 18:04:24 +0300

    добавила пометки в задаче 9

--- task05/Program.cs
+++ task05/Program.cs
@@ -6,7 +6,7 @@
 // 85 -> 8
 
 int number = new Random().Next(10, 100);
-Console.WriteLine($"случайное число из отрезка 10-99 -> {number}");
+Console.WriteLine($"случайное число из отрезка 10-99 -> {number}");    //получение числа
 
 // int firstDigit = number / 10;
 // int secondDigit = number % 10;
@@ -16,17 +16,22 @@ Console.WriteLine($"случайное число из отрезка 10-99 -> {
 //     Console.WriteLine($"Наибольшая цифра сисла -> {secondDigit}");
 
 // ///////////////////////////////
-// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;     //тернарный оператор
 // Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
 // ///////////////////////////////
 int result = MaxDigit(number);
-Console.WriteLine($"Наибольшая цифра числа -> {result}");
+Console.WriteLine($"Наибольшая цифра числа -> {result}");    //метод
+
+// int result1 = MaxDigit(68);    //можно одновременно с той же функцией вызывать несколько чисел и с набором вручную
+// Console.WriteLine($"Наибольшая цифра числа -> {result1}");
 
 int MaxDigit(int num)
 
 {
-    int firstDigit = number / 10;
-    int secondDigit = number % 10;
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
     int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
     return maxDigit;
 }
+
+

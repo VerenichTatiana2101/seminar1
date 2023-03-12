43a8ee1ae1f071fa79657e1853305b5768234a34
Verenich Tatiana <verenik91@gmail.com>
2023-03-12 03:32:55 +0300

    добавлена задача 15

---
+++ dz15/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 15: Напишите программу, которая 
+// принимает на вход цифру, 
+// обозначающую день недели, и 
+// проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number == 1)
+// {
+//     Console.Write($"День недели {number} - нет");
+// }
+
+// else if (number == 2)
+// {
+//     Console.Write($"День недели {number} - нет");
+// }
+
+// else if (number == 3)
+// {
+//     Console.Write($"День недели {number} - нет");
+// }
+
+// else if (number == 4)
+// {
+//     Console.Write($"День недели {number} - нет");
+// }
+
+// else if (number == 5)
+// {
+//     Console.Write($"День недели {number} - нет");
+// }
+
+// else if (number == 6)
+// {
+//     Console.Write($"День недели {number} - да");
+// }
+
+// else if (number == 7)
+// {
+//     Console.Write($"День недели {number} - да");
+// }
+
+// else
+// {
+//     Console.Write($"Дня недени {number} - не существует, введите другое число");
+// }
+
+if (number < 8 && number > 0)
+{
+    if (number < 6)
+    {
+        Console.Write($"День недели {number} - нет");
+    }
+    if (number > 5)
+        Console.Write($"День недели {number} - да");
+}
+else
+    Console.Write($"Дня недени {number} - не существует, введите другое число");
+
+
+

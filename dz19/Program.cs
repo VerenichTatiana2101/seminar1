470751fc4a41102444de286c7df261917388363c
Verenich Tatiana <verenik91@gmail.com>
2023-03-16 00:40:04 +0300

    Добавлена задача 19

---
+++ dz19/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 19
+// Напишите программу, которая 
+// принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций 
+// (целочисленное деление, остаток от деления).
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Console.WriteLine("Введите пятизначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int result = Revers(number);
+// bool rez = Polidrom(number);
+
+// if (number >= 99999 && number < 10000)
+//     Console.WriteLine($"Число не пятизначное");
+
+
+// Console.WriteLine(rez ? "Да" : "Нет");
+// bool Polidrom(int revers)
+// {
+
+//     if (number > 1000 && number < 100000)
+//         if (number == Revers(number))
+//             return true;
+//     return false;
+// }
+
+// int Revers(int num)
+// {
+//     return num % 10 * 10000
+//     + (num / 10) % 10 * 1000
+//     + (num / 100) % 10 * 100
+//     + (num / 1000) % 10 * 10
+//     + (num / 10000);
+// }
+
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool rezult = Polidrom(number);
+
+if (number > 1000 && number < 100000)
+{
+    Console.WriteLine(rezult ? "Да" : "Нет");
+}
+else
+    Console.WriteLine($"Число не пятизначное");
+
+bool Polidrom(int revers)
+{
+    if (number < 0) number = number * -1;
+    if (number == Revers(number))
+        return true;
+    return false;
+}
+
+int Revers(int num)
+{
+    return num % 10 * 10000
+    + (num / 10) % 10 * 1000
+    + (num / 100) % 10 * 100
+    + (num / 1000) % 10 * 10
+    + (num / 10000);
+}

9dc72cd729a3d7f706b442ed5e1eff82a64af349
Verenich Tatiana <verenik91@gmail.com>
2023-03-18 00:44:53 +0300

    добавлены задачи 22,28,

---
+++ task16/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите целое  число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factNumbers = FactNumbers(number);
+Console.WriteLine($"сумма чисел от 1 до {number} -> {factNumbers}");
+
+int FactNumbers(int num)
+{
+    int product = 1;
+    for (int count = 2; count <= num; count++)
+    {
+        checked
+        {
+            product = product * count;
+        }
+
+    }
+    return product;
+}
+
---
+++ task17/Program.cs
@@ -0,0 +1,59 @@
+﻿// int[] array = new int[8]; // говорим компилятору сколько нужно выделить памяти  каждый 4 байта, тоесть в сумме 32 байта. 
+// // 8 это колличество элементов 0,1,2,3,4,5,6,7
+// array[3] = 34;
+
+
+// int[] array = new int[8];
+// array[3] = 34;
+// for (int i = 0; i < array.Length; i++)
+// {
+//   array[i] = new Random().Next(0,2);
+//   Console.Write(array[i] + " "); 
+// }
+
+// // int[] arr = { 1, 2, 3 } // можно и так, но иногда нужен пустой
+
+
+
+
+
+// int[] array = new int[8];
+
+// void FillArr(int[] arr)
+// {
+//   for (int i = 0; i < array.Length; i++)
+// {
+//   arr[i] = new Random().Next(0,2);
+// }
+// }
+
+// void PrintArr(int[] arr)
+// {
+// for (int i = 0; i < array.Length; i++)
+// Console.Write(array[i] + " ");
+// }
+
+// FillArr(array);
+// PrintArr(array);
+
+
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+
+    {
+        Console.Write(array[i] + " ");
+    }
+
+}

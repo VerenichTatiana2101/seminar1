ab0a6e4c217fd7b45b62bac7ad4402ca98208df5
Verenich Tatiana <verenik91@gmail.com>
2023-03-18 02:18:50 +0300

    добавлены задачa 25

---
+++ dz25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите целое  число 1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое  число 2: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+int degreeNumbers = DegreeNumber(number, degree);
+
+Console.WriteLine($"Число {number} в степени {degree} -> {degreeNumbers}");
+
+int DegreeNumber(int num, int degr)
+{
+    int num1 = num;
+    for (int count = 2; count <= degr; count++)
+    {
+        num *= num1;   
+    }
+    return num;
+}

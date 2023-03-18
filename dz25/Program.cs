35aaa0e72854c1c7959db31ef3c15a3406a20e3c
Verenich Tatiana <verenik91@gmail.com>
2023-03-18 15:13:39 +0300

    Добавлены наброски домашней задачи 25

--- dz25/Program.cs
+++ dz25/Program.cs
@@ -1,18 +1,27 @@
-﻿Console.WriteLine("Введите целое  число 1: ");
+﻿// Задача 25: Напишите цикл, который 
+// принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите целое  число 2: ");
+Console.WriteLine("Введите степень числа: ");
 int degree = Convert.ToInt32(Console.ReadLine());
 
-int degreeNumbers = DegreeNumber(number, degree);
-
-Console.WriteLine($"Число {number} в степени {degree} -> {degreeNumbers}");
+int degreeNumber = DegreeNumber(number, degree);
+Console.WriteLine($"Число {number} в степени {degree} -> {degreeNumber}");
 
 int DegreeNumber(int num, int degr)
 {
-    int num1 = num;
-    for (int count = 2; count <= degr; count++)
+    int numFix = num;
+    for (int i = 2; i <= degr; i++)
     {
-        num *= num1;   
+        checked
+        {
+            num *= numFix;
+        }
+
     }
     return num;
 }

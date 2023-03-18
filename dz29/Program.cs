c53a34cb1f46c53c2a3c7091eba10da86e4390c9
Verenich Tatiana <verenik91@gmail.com>
2023-03-19 01:00:33 +0300

    удалён черновик

---
+++ dz29/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 29: Напишите программу, которая 
+// задаёт массив из 8 элементов и 
+// выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+
+if (arraySize <= 0)
+{
+    Console.WriteLine($"Ошибка: введено неверное значение {arraySize}");
+    return; 
+}
+
+int[] arr = FillArray(arraySize);
+PrintArray(arr);
+
+int[] FillArray(int arrayLength)
+{
+    int[] newArray = new int[arrayLength];
+    Random random = new Random();
+    for (int i = 0; i < arrayLength; i++)
+    {
+        newArray[i] = random.Next(1, 100);
+    }
+    return newArray;
+}
+
+void PrintArray(int[] array)
+{
+    int i = 0;
+    Console.Write("[");
+    while (i < array.Length)
+    {
+        Console.Write(array[i]);
+        i++;
+        if (i < array.Length)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+

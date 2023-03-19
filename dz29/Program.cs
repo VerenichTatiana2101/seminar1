0370e8252a932fd9582b58671c7e76999e76640c
Verenich Tatiana <verenik91@gmail.com>
2023-03-19 15:21:51 +0300

    Добавлено решение строго по условию

--- dz29/Program.cs
+++ dz29/Program.cs
@@ -3,27 +3,57 @@
 // выводит их на экран.
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
-Console.Write("Введите размер массива: ");
-int arraySize = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите размер массива: ");
+// int arraySize = Convert.ToInt32(Console.ReadLine());
 
-if (arraySize <= 0)
-{
-    Console.WriteLine($"Ошибка: введено неверное значение {arraySize}");
-    return; 
-}
+// if (arraySize <= 0)
+// {
+//     Console.WriteLine($"Ошибка: введено неверное значение {arraySize}");
+//     return; 
+// }
 
-int[] arr = FillArray(arraySize);
+// int[] arr = FillArray(arraySize);
+// PrintArray(arr);
+
+// int[] FillArray(int arrayLength)
+// {
+//     int[] newArray = new int[arrayLength];
+//     Random random = new Random();
+//     for (int i = 0; i < arrayLength; i++)
+//     {
+//         newArray[i] = random.Next(1, 100);
+//     }
+//     return newArray;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     int i = 0;
+//     Console.Write("[");
+//     while (i < array.Length)
+//     {
+//         Console.Write(array[i]);
+//         i++;
+//         if (i < array.Length)
+//         {
+//             Console.Write(", ");
+//         }
+//     }
+//     Console.Write("]");
+// }
+
+int[] arr = new int[8];
+
+FillArray(arr);
 PrintArray(arr);
 
-int[] FillArray(int arrayLength)
+void FillArray(int[] array)
 {
-    int[] newArray = new int[arrayLength];
-    Random random = new Random();
-    for (int i = 0; i < arrayLength; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        newArray[i] = random.Next(1, 100);
+        Random random = new Random(); 
+        array[i] = random.Next(1, 100);
     }
-    return newArray;
 }
 
 void PrintArray(int[] array)
@@ -41,4 +71,3 @@ void PrintArray(int[] array)
     }
     Console.Write("]");
 }
-

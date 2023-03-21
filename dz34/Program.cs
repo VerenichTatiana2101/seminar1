101af15f220017887f3407edcb3786930e72ee93
Verenich Tatiana <verenik91@gmail.com>
2023-03-21 19:21:17 +0300

    Добавлены наброски задачи 34, работает

---
+++ dz34/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 34: 
+// Задайте массив 
+// заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая 
+// покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = ArrayRndInt(5, 99, 1000);
+PrintArray(array);
+
+int sumNumber = SumEvenNumbers(array);
+Console.WriteLine();
+Console.WriteLine($"Количество чётных чисел в массиве: {sumNumber}");
+
+int[] ArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int SumEvenNumbers(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            sum++;
+    }
+    return sum;
+}
+
+
+

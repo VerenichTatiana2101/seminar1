d77c9b0ae550448a0f1518836e0c0bdb62058360
Verenich Tatiana <verenik91@gmail.com>
2023-03-21 16:24:31 +0300

    решение задачи 37

--- task22/Program.cs
+++ task22/Program.cs
@@ -6,7 +6,7 @@
 
 Console.Clear();
 
-int[] array = CreateArrayRndInt(15, 3, 5);
+int[] array = CreateArrayRndInt(5, 1, 5);
 PrintArray(array);
 
 int[] newArr = Multiplication(array);
@@ -54,15 +54,3 @@ void PrintArray(int[] arr)
 }
 
 
-double[] CreateArrayRndDouble(int size, int min, int max)
-{
-    double[] arr = new double[size];
-    Random rnd = new Random();
-
-    for (int i = 0; i < arr.Length; i++)
-    {
-        double num = rnd.NextDouble() * (max - min) + min;
-        arr[i] = Math.Round(num, 1);
-    }
-    return arr;
-}

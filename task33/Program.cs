88ee91b2312b21b421136aa06e74b26f793f12ad
Verenich Tatiana <verenik91@gmail.com>
2023-03-31 20:43:25 +0300

    Добавлена задача 53

---
+++ task33/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 53.
+// Задайте двумерный массив. Напишите программу,
+// которая поменяет первую и последнюю строки массива.
+
+int[,] array2d = CreateMatrixRndInt(4, 5, -10, 10);
+PrintMatrix(array2d);
+Console.WriteLine();
+SwappingStringsMatrix(array2d);
+PrintMatrix(array2d);
+
+void SwappingStringsMatrix(int[,] matrix)           //метод замены строчек массива местами
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{                          // 0       1
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+
+
+
+
+
+
+
+
